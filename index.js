a5cd78fbd5ba0e443f1515f913a01b3c3744813d
Anha Mehta <anhamehta@dynamic-oit-ip4-wifirestricted-10-29-114-135.princeton.org>
2025-03-29 22:57:31 -0400

    animated popup

--- index.js
+++ index.js
@@ -82,10 +82,12 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
+    const redirectUrl = req.query.redirect || '/dashboard'; // Default to '/dashboard' if no redirect param
+
     if (req.session.username) {
-        return res.redirect('/dashboard'); // Redirect if already logged in
+        return res.redirect(redirectUrl); // Redirect if already logged in
     }
-    res.render('login');
+    res.render('login', { redirectUrl });
 });
 
 // Handle login (set session)
